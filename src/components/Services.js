ca6e413129605dd733d24b6756f3df1294536151
iamisafi <isafi@gmail.com>
2019-11-22 13:08:24 +0600

    react resort app

---
+++ src/components/Services.js
@@ -0,0 +1,46 @@
+import React, { Component } from "react";
+import { FaCocktail, FaHiking, FaShuttleVan, FaBeer } from "react-icons/fa";
+import Title from "./Title";
+
+export default class Services extends Component {
+ state={
+   services: [
+     {
+       icon:<FaCocktail/>,
+       title:"Free cocktails",
+       info: 'Our Services is world class!'
+     },
+     {
+       icon:<FaHiking/>,
+       title:"Endless Hiking",
+       info: 'Our Services is world class!'
+     },
+     {
+       icon:<FaShuttleVan/>,
+       title:"Free Shuttle",
+       info: 'Our Services is world class!'
+     },
+     {
+       icon:<FaBeer/>,
+       title:"Strongest Beer",
+       info: 'Our Services is world class!'
+     }
+   ]
+ }
+ render() {
+    return (
+      <section className="services">
+      <Title title="services" />
+      <div className="services-center">
+       {this.state.services.map((item,index) => {
+         return <article key={index} className="service">
+         <span>{item.icon}</span>
+         <h6>{item.title}</h6>
+         <p>{item.info}</p>
+         </article>
+       })}
+       </div>
+       </section>
+    );
+  }
+}
